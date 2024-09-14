61b686219d8c5d94555924933b938879c3a69a00
Rafael Guimarães <rafaelvg1992@gmail.com>
2024-09-14 00:23:20 -0300

    corrige erros na busca junto ao site

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import json
-
-# - inicializa o arquivo json no qual será salva consulta
-with open ('data_amazon.json', 'w') as f:
+ 
+with open('data_amazon.json', 'w') as f:
     json.dump([], f)
     
 def write_json(new_data, filename='data_amazon.json'):
@@ -15,37 +14,33 @@ def write_json(new_data, filename='data_amazon.json'):
         file.seek(0)
         json.dump(file_data, file, indent=4)
 
-elemToSearch = input("Informe o item que deseja pesquisar: ")
-# inicialização do webdriver
+elemToSearch = input("Informe o elemento que deseja procurar: ")
+# 1- Utilização do WebDriver
 browser = webdriver.Firefox()
 browser.get('https://www.amazon.com.br')
 
-# entrando de forma dinamica com o elemento a ser pesquisado na Amazon
-
+# 2 - Acessando elemento de pesquisa
 elem = browser.find_element(By.ID, 'twotabsearchtextbox')
-elem.send_keys('xbox')
-time.sleep(2)
+elem.send_keys(elemToSearch)
 elem.send_keys(Keys.ENTER)
 time.sleep(2)
 
-# inicializa a procura por todas as páginas da busca no site
 isNextDisabled = False
 while not isNextDisabled:
-    # encontrando os elementos de todos os resultados
-    element = browser.find_element(
-    By.CSS_SELECTOR,
-    'div.s-main-slot.s-result-list.s-search-results.sg-row'
+    # 3 - Encontrando os elementos de todos os resultados
+    element = browser.find_element(By.CSS_SELECTOR,
+        'div.s-main-slot.s-result-list.s-search-results.sg-row'
     )
     print(element)
     time.sleep(2)
     
-    # encontrando informações dos produtos procurados
+    # 4 - Encontrar informações dos produtos
     items = element.find_elements(
         By.XPATH,
         '//div[@data-component-type="s-search-result"]'
     )
     print(len(items))
-    
+
     for item in items:
         title = item.find_element(By.TAG_NAME, 'h2').text
         price = ""
@@ -59,9 +54,9 @@ while not isNextDisabled:
             price = item.find_element(
                 By.CLASS_NAME,
                 'a-price'
-            ).text.replace('\n', '.') # substitui o espaço em branco por "."
+            ).text.replace('\n', '.')
         except:
-            pass
+            pass    
         
         try:
             img = item.find_element(
@@ -74,21 +69,21 @@ while not isNextDisabled:
         print(f"Título: {title}")
         print(f"Preço: {price}")
         print(f"Link: {link}")
-        print(f"Título: {img}")
+        print(f"Image: {img}")
         
         write_json(
             {
                 "title": title,
                 "price": price,
                 "link": link,
-                "image": img,
+                "image": img
             }
         )
-        
+    
     try:
         next_btn = browser.find_element(
             By.CLASS_NAME,
-           's-pagination-next'
+            's-pagination-next'
         )
         next_class = next_btn.get_attribute('class')
         if 's-pagination-disabled' in next_class:
